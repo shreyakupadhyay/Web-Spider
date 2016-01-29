da1ccc1f932d245fcc4af3ec3fec4cfa209ea5b2
shreyakupadhyay <shreyakupadhyay07@gmail.com>
2016-01-29 18:55:44 +0530

    scraping yahoo finance for stocks and storing in database

---
+++ finance_stocks.py
@@ -0,0 +1,44 @@
+from threading import Thread
+import urllib
+import re
+import MySQLdb
+
+gmap ={}
+
+def th(ur):
+    url_base = "http://finance.yahoo.com/q?s="+ur
+    htmltext = urllib.urlopen(url_base).read()
+    regex = '<span id="yfs_l84_' + ur.lower() + '">(.+?)</span>'
+    pattern = re.compile(regex)
+    results = re.findall(pattern,htmltext)
+    gmap[ur] = results[0]
+    try:
+        print "the price of " + str(ur) + " is " + str(results)
+    except:
+        print "got an error"
+
+symbollist = open("symbol.txt").read()
+symbollist = symbollist.replace("\n",",").split(",")
+
+threadlist = []
+
+
+for sym in symbollist:
+    t = Thread(target=th,args=(sym,))
+    t.start()
+    threadlist.append(t)
+
+for sym_th in threadlist:
+    sym_th.join() 
+
+
+connect_database = MySQLdb.connect(host="127.0.0.1",user="root",passwd="BlackHole",db="learning_for_scraping")
+
+for key in gmap.keys():
+    print key,gmap[key]
+    query = "INSERT INTO about_stocks  (username,stocks_value) values (" 
+    query = query + "'" + key + "'," + gmap[key] + ")"
+    x = connect_database.cursor()  #this line is for selecting from the database;
+    x.execute(query) #this line is used to execute a query
+    row = x.fetchall()  #this line is to fetch data from the databse
+    connect_database.commit() 
